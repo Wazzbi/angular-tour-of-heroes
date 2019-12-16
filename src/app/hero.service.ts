bed7525ed5d1e52e259d78c1fe0d7e08f01fad94
danovotny <danovotny@trask.cz>
2019-12-16 12:49:19 +0100

    toh-pt4-DONE

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,3 +1,4 @@
+import { MessageService } from "./message.service";
 import { HEROES } from "./mock-heroes";
 import { HeroComponent } from "./hero/hero.component";
 import { Injectable } from "@angular/core";
@@ -8,8 +9,10 @@ import { Observable, of } from "rxjs";
 })
 export class HeroService {
   getHeroes(): Observable<HeroComponent[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
   }
 
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 }
