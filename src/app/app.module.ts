026aaa0d3ec04e0887db8b3ba59bfac2bf9b5105
danovotny <danovotny@trask.cz>
2019-12-18 15:59:19 +0100

    toh-pt6-returns response data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,35 @@ import { HeroesComponent } from "./heroes/heroes.component";
 import { HeroComponent } from "./hero/hero.component";
 import { FormsModule } from "@angular/forms";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
-import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent } from "./messages/messages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      HeroesComponent,
-      HeroComponent,
-      HeroDetailComponent,
-      MessagesComponent,
-      DashboardComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      FormsModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false
+    }),
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { HEROES } from "./mock-heroes";
 import { HeroComponent } from "./hero/hero.component";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
-
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 @Injectable({
   providedIn: "root"
 })
@@ -13,7 +13,7 @@ export class HeroService {
     this.messageService.add(
       `${new Date().toLocaleTimeString()} \xa0\xa0 HeroService: fetched heroes`
     );
-    return of(HEROES);
+    return this.http.get<HeroComponent[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<HeroComponent> {
     // TODO: send the message _after_ fetching the hero
@@ -23,5 +23,15 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) {}
+  /* Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = "api/heroes"; // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 }
